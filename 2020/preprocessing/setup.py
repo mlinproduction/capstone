2ca49ee65c7bb07b19eaff900ad627c6f9649725
atibaup <arnau.tibau@letgo.com>
2020-02-15 11:39:34 +0100

    adding sklearn as a dependency of pre-processing

--- 2020/preprocessing/setup.py
+++ 2020/preprocessing/setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'numpy >=1.16.0, <2.0',
         'tensorflow >= 2.0, < 3',
-        'transformers == 2.4.1'
+        'transformers == 2.4.1',
+        'scikit-learn >= 0.22, < 1.0'
     ]
 )
