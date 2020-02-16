134f31dd3b83328c8fac2db17a0cdc6163af405c
atibaup <arnau.tibau@letgo.com>
2020-02-16 19:45:45 +0100

    getting version through https://github.com/pypa/setuptools_scm

--- 2020/preprocessing/setup.py
+++ 2020/preprocessing/setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
     name='preprocessing',
-    version='1.0',
     description='NLP preprocessing',
     author='DS in Prod',
     install_requires=[
@@ -11,5 +10,6 @@ setup(
         'transformers == 2.4.1',
         'scikit-learn >= 0.22, < 1.0',
         'pandas >=1.0.1, < 1.1'
-    ]
+    ],
+    use_scm_version=True
 )
