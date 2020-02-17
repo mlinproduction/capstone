06ae95aeba0c11405c059a0689ddcf3ad3574e7c
Bernat Garcia <bgarcia@yaencontre.com>
2020-02-17 23:12:22 +0100

    Added tag_id to flattened_tags

--- 2020/dags/sql/flatten_tags.sql
+++ 2020/dags/sql/flatten_tags.sql
@@ -15,6 +15,7 @@ WITH
     t1.id AS post_id,
     IFNULL(t2.tag_name,
       '(other)') AS tag_name,
+    t2.tag_id,
     tag_position
   FROM
     tagged_posts AS t1
@@ -31,12 +32,14 @@ WITH
   SELECT
     post_id,
     tag_name,
-    MIN(tag_position) AS tag_position
+    MIN(tag_position) AS tag_position,
+    tag_id
   FROM
     flattened_and_filtered_tags
   GROUP BY
     post_id,
-    tag_name)
+    tag_name,
+    tag_id)
 SELECT
   * EXCEPT(tag_position),
   ROW_NUMBER() OVER(PARTITION BY post_id ORDER BY tag_position) AS tag_position
